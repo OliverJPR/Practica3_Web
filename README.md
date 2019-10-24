8e893275462eb80943ae7f36cabc3508465edc86
OliverJPR <33287824+OliverJPR@users.noreply.github.com>
2019-10-24 00:00:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web-Practica3
+ 
---
+++ src/main/java/DB/Conexion.java
@@ -0,0 +1,19 @@
+package DB;
+
+import org.sql2o.Sql2o;
+
+public class Conexion {
+
+    private static Sql2o sql2o = null;
+
+    public Sql2o getConexion() {
+
+        sql2o = new Sql2o("jdbc:h2:~/test", "sa", "");
+
+        return sql2o;
+
+    }
+
+
+
+}
